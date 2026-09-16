91505b6ebc72c04ecb7aa749511c58dce766afae
benk-bytesnap <benk@bytesnap.co.uk>
2026-09-16 16:33:40 +0100

    WIP

--- imx8m/dart_mx8mp/SharedBoardConfig.mk
+++ imx8m/dart_mx8mp/SharedBoardConfig.mk
@@ -58,9 +58,9 @@ BOARD_VENDOR_KERNEL_MODULES += \
     $(KERNEL_OUT)/drivers/net/phy/realtek.ko \
     $(KERNEL_OUT)/drivers/ptp/ptp.ko \
     $(KERNEL_OUT)/drivers/pps/pps_core.ko \
+    $(KERNEL_OUT)/drivers/net/wireless/broadcom/brcm80211/brcmutil/brcmutil.ko \
+    $(KERNEL_OUT)/drivers/net/wireless/broadcom/brcm80211/brcmfmac/brcmfmac.ko \
     $(KERNEL_OUT)/drivers/net/ethernet/freescale/fec.ko
-#    $(KERNEL_OUT)/drivers/net/wireless/broadcom/brcm80211/brcmutil/brcmutil.ko \
-#    $(KERNEL_OUT)/drivers/net/wireless/broadcom/brcm80211/brcmfmac/brcmfmac.ko \
 else
 BOARD_VENDOR_KERNEL_MODULES +=     \
     $(KERNEL_OUT)/drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_i2c.ko
